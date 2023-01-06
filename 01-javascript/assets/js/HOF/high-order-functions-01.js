7008ca2cf5b2508edf45ba9c55cdd46d77a32f3c
Veysel BIGEC <veyselbgc@gmail.com>
2023-01-05 23:47:49 -0500

    02

--- 01-javascript/assets/js/HOF/high-order-functions-01.js
+++ 01-javascript/assets/js/HOF/high-order-functions-01.js
@@ -1,3 +1,56 @@
-const numbers = [1, -1, 2, 3] ;
-const filtered = numbers.filter(num => num >= 0);
-console.log(filtered);
+// filter & map
+
+// a)
+
+// const numbers = [1, -1, 2, 3] ;
+// const filtered = numbers.filter(num => num >= 0);
+// console.log(filtered);        // gives us [ 1, 2, 3 ]
+
+// b)
+
+// const numbers = [1, -1, 2, 3] ;
+// const filtered = numbers.filter(num => num >= 0);
+// const items = filtered.map(num => '<li>' + num + '</li>') ;
+// console.log(items);        // gives us 
+// //           [ '<li>1</li>', '<li>2</li>', '<li>3</li>' ]
+
+// c)
+
+// const numbers = [1, -1, 2, 3] ;
+// const filtered = numbers.filter(num => num >= 0);
+// const items = filtered.map(num => '<li>' + num + '</li>') ;
+// const html = items.join();  // by default it is comma!
+// console.log(html);        // gives us 
+// //           <li>1</li>,<li>2</li>,<li>3</li>
+
+// d) 1)
+
+// const numbers = [1, -1, 2, 3] ;
+// const filtered = numbers.filter(num => num >= 0);
+// const items = filtered.map(num => {
+//     const obj = {value: num } ;
+//     return obj;
+// });
+// console.log(items);        // gives us 
+//           [ { value: 1 }, { value: 2 }, { value: 3 } ]
+
+// d) 2)
+
+// const numbers = [1, -1, 2, 3] ;
+// const filtered = numbers.filter(num => num >= 0);
+// const items = filtered.map(num => {
+//    return {value: num } ;   
+// });
+// console.log(items);        // gives us 
+//           [ { value: 1 }, { value: 2 }, { value: 3 } ]
+
+// d) 3)
+
+// const numbers = [1, -1, 2, 3] ;
+// const filtered = numbers.filter(num => num >= 0);
+// const items = filtered.map(num => ({value: num }));
+// console.log(items);        // gives us 
+//           [ { value: 1 }, { value: 2 }, { value: 3 } ]
+
+
+
